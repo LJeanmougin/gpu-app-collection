d6c9727fa52086fff46d738d8449b84443f184e0
LJeanmougin <louison.jeanmougin@irit.fr>
2025-05-03 16:54:41 +0200

    Splitted sgemm into different versions

---
+++ src/cuda/sgemm_32w/sgemm_config.h
@@ -0,0 +1,8 @@
+#ifndef SGEMM_CONFIG_H
+#define SGEMM_CONFIG_H
+
+#define BLOCKSIZE 32
+#define WARP_COUNT 32
+#define LINE_SIZE 1024
+
+#endif
---
+++ src/cuda/sgemm_8w/sgemm_config.h
@@ -0,0 +1,8 @@
+#ifndef SGEMM_CONFIG_H
+#define SGEMM_CONFIG_H
+
+#define BLOCKSIZE 32
+#define WARP_COUNT 8
+#define LINE_SIZE 1024
+
+#endif
