469e2ee6984a1e6ebb4a75043a0c4e8d1d0a6cef
LJeanmougin <louison.jeanmougin@irit.fr>
2025-08-26 10:28:22 +0200

    Separated rodinia_1w from rodinia_1b. Re-hosted data_dirs to be independant from purdue

--- get_data.sh
+++ get_data.sh
@@ -4,7 +4,7 @@ DATA_SUBDIR="/data_dirs/"
 DATA_ROOT=$BASH_ROOT$DATA_SUBDIR
 
 if [ ! -d $DATA_ROOT ]; then
-    wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/benchmark_data/all.gpgpu-sim-app-data.tgz
-    tar xzvf all.gpgpu-sim-app-data.tgz -C $BASH_ROOT
-    rm all.gpgpu-sim-app-data.tgz
+    wget https://cloud.irit.fr/s/dCPHpY2u6vDMQFV/download
+    tar xf download -C $BASH_ROOT
+    rm download
 fi
